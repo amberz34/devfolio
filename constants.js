3926b0e5c675b3e50a0bfc5b715762cfaa69e75d
Mrhamza01 <hamza.kamboh035@gmail.com>
2024-05-19 19:44:31 +0500

    site link changed & build deleted

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ export const METADATA = {
   title: "Portfolio | hamza ghafoor",
   description:
     "My name is Muhammad hamza. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript and typescript. I am available for any kind of job opportunity that suits my skills and interests.",
-  siteUrl: "https://www.shubhporwal.me/",
+  siteUrl: "https://hamzaghafoor.vercel.app/",
   twitterHandle: "@hamzakambo33483",
   keywords: [
     "hamza ghafoor",
